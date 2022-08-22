08abf2bdee68f095d224255f4de22b6e9ca7c9a3
Ben McMullin <46494991+benmcmullin@users.noreply.github.com>
2022-08-22 09:23:32 -0500

    Add files via upload

---
+++ Class 2 Exercise/Ex 1/main.js
@@ -0,0 +1,9 @@
+const str1 = "Hello";
+const number = 25;
+const boolean1 ="true";
+const null1 = (0 / 4);
+const undef1 = "";
+const obj1 = {};
+const array1 = [Ford, Jeep, Toyota];
+const array2 = array1[0];
+let x;
---
+++ Class 2 Exercise/Ex 2/main.js
@@ -0,0 +1,3 @@
+const food1 = 45
+const food2 = 25
+const food3 = 30
