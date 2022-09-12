33b093286449232815f09fc4a8119c90ded43898
Ben McMullin <46494991+benmcmullin@users.noreply.github.com>
2022-09-12 14:19:25 -0500

    Add files via upload

---
+++ Class 2 Exercise/Ex 3/main.js
@@ -0,0 +1,11 @@
+function arrayOfMultiples (num1, num2) {
+    let arrOne = [];
+    arrOne.push(num1);
+    arrOne.length = num2;
+    for (let i = 0; i <= arrOne.length; i++){
+        
+        // const result= i*num1;
+    }
+    console.log(arrOne);
+}
+arrayOfMultiples(7,5);
